201497ec9e2629c1e421574871a19e300af035c6
marypaduano <maria.paduano@gmail.com>
2023-01-31 18:27:58 +0100

    feat discount price

--- js/style.js
+++ js/style.js
@@ -5,6 +5,9 @@ console.log(firstName)
 
 let lastName = prompt ('inserisci il tuo Cognome')
 console.log(lastName)
+ 
+let discount = 0
+
 
 
 //chiedere all'utente quanti km deve percorrere
@@ -25,5 +28,22 @@ console.log('il prezzo totale per i km da percorrere è:', totalPrice)
 
 //sconto 20% < età 18
 //sconto 40% > età 65
+
+
+if (yearsOld < 18) {
+    discount = totalPrice * 0.2 
+    discount = discount.toFixed(2);
+    let discountPrice = totalPrice - discount  
+    console.log('il prezzo totale scontato del 20% è di €', discount) 
+}
+else if (yearsOld > 65) {
+    discount = totalPrice * 0.4
+    discount = discount.toFixed(2); 
+    let discountPrice = totalPrice - discount
+    console.log('il prezzo totale scontato del 40% è di €', discount)
+}
+else {    
+   console.log('non sono previsti sconti per questo biglietto')
+}
 
 //stampare a video prezzo totale con arrotondamento centesimi
