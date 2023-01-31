84c4fc7cc5b9cd144aa7fcffa2e9db51b5f051c8
marypaduano <maria.paduano@gmail.com>
2023-01-31 19:25:33 +0100

    added total to screen

--- js/style.js
+++ js/style.js
@@ -6,10 +6,6 @@ console.log(firstName)
 let lastName = prompt ('inserisci il tuo Cognome')
 console.log(lastName)
  
-let discount = 0
-
-
-
 //chiedere all'utente quanti km deve percorrere
 let km = prompt ('Quanti kilometri devi percorrere?')
 km = parseFloat(km)
@@ -24,6 +20,7 @@ console.log(yearsOld)
 //prezzo biglietto €0.21/km
 const priceKm = 0.21;
 let totalPrice = km * priceKm
+totalPrice = totalPrice.toFixed(2)
 console.log('il prezzo totale per i km da percorrere è:', totalPrice)
 
 //sconto 20% < età 18
@@ -32,18 +29,24 @@ console.log('il prezzo totale per i km da percorrere è:', totalPrice)
 
 if (yearsOld < 18) {
     discount = totalPrice * 0.2 
-    discount = discount.toFixed(2);
+    discount = discount.toFixed(2)
     let discountPrice = totalPrice - discount  
-    console.log('il prezzo totale scontato del 20% è di €', discount) 
+    console.log('il prezzo totale scontato del 20% è di €', discountPrice) 
+    let prezzoFinale = document.getElementById('price')
+    prezzoFinale.innerHTML = discountPrice
 }
 else if (yearsOld > 65) {
     discount = totalPrice * 0.4
-    discount = discount.toFixed(2); 
+    discount = discount.toFixed(2) 
     let discountPrice = totalPrice - discount
-    console.log('il prezzo totale scontato del 40% è di €', discount)
+    console.log('il prezzo totale scontato del 40% è di €', discountPrice)
+    let prezzoFinale = document.getElementById('price')
+    prezzoFinale.innerHTML = discountPrice
 }
 else {    
    console.log('non sono previsti sconti per questo biglietto')
+   let prezzoFinale = document.getElementById('price')
+   prezzoFinale.innerHTML = totalPrice
 }
 
 //stampare a video prezzo totale con arrotondamento centesimi
