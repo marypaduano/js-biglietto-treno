748e73a6f95e97c65b3db44b094dacdb422283d1
marypaduano <maria.paduano@gmail.com>
2023-01-31 18:02:07 +0100

    feat total price

--- js/style.js
+++ js/style.js
@@ -1,1 +1,29 @@
 console.log('works!')
+
+let firstName = prompt('Inserisci il tuo Nome')
+console.log(firstName)
+
+let lastName = prompt ('inserisci il tuo Cognome')
+console.log(lastName)
+
+
+//chiedere all'utente quanti km deve percorrere
+let km = prompt ('Quanti kilometri devi percorrere?')
+km = parseFloat(km)
+console.log(km)
+
+//chiedere la sua età
+let yearsOld = prompt ('Quanti anni hai?')
+yearsOld = parseInt(yearsOld)
+console.log(yearsOld)
+
+//RULES
+//prezzo biglietto €0.21/km
+const priceKm = 0.21;
+let totalPrice = km * priceKm
+console.log('il prezzo totale per i km da percorrere è:', totalPrice)
+
+//sconto 20% < età 18
+//sconto 40% > età 65
+
+//stampare a video prezzo totale con arrotondamento centesimi
